9fcc509661ca96c47610a8efa78df43a5da0846e
Sam Pullman <sampullman@gmail.com>
2021-02-26 12:22:46 +0800

    Update for release
    Package name, main, module, exports, dist files, etc

--- src/index.js
+++ src/index.js
@@ -28,17 +28,4 @@ const VueCharts = {
   render: () => console.error('[vue-chartjs]: This is not a vue component. It is the whole object containing all vue components. Please import the named export or access the components over the dot notation. For more info visit https://vue-chartjs.org/#/home?id=quick-start')
 }
 
-export {
-  VueCharts,
-  Bar,
-  HorizontalBar,
-  Doughnut,
-  Line,
-  Pie,
-  PolarArea,
-  Radar,
-  Bubble,
-  Scatter,
-  mixins,
-  generateChart
-}
+export default VueCharts;
